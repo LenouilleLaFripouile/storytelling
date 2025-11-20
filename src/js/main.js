7fabccc3b9b0059ffd59980a0f0426ee5c4bfe8d
LenouilleLaFripouile <161487400+LenouilleLaFripouile@users.noreply.github.com>
2025-11-20 08:55:16 +0100

    ajout html css

--- src/js/main.js
+++ src/js/main.js
@@ -14,67 +14,56 @@ window.addEventListener("DOMContentLoaded", function () {
   const sysSupVid = document.querySelector(".sys-supp-vid");
   const sysErr = document.querySelector(".sys-supp-err");
 
-  sysClickInitButton.addEventListener("click", function () {
-    document.querySelector("body").style.overflow = "";
-    sysClickInit.classList.add("hidden");
-    sysClickVid.classList.remove("sys-init-vid-hidden");
-  });
-
-  const tlintro = gsap.timeline({
-    scrollTrigger: {
-      trigger: sysContainer,
-      start: "top",
-      end: "300%",
-      scrub: true,
-      pin: true,
-      markers: true,
-    },
-  });
-
-  if (sysClickVid.duration) {
-    tlintro.to(sysClickVid, { currentTime: sysClickVid.duration }).call(() => {
-      gsap.to(sysClickVid, {
-        opacity: 0,
-      });
-      sysClickSupp.classList.remove("sys-click-supp-hidden");
-      document.querySelector("body").style.overflow = "hidden";
+  sysSupVid.addEventListener("loadeddata", (e) => {
+    sysClickInitButton.addEventListener("click", function () {
+      document.querySelector("body").style.overflow = "";
+      sysClickInit.classList.add("hidden");
+      sysClickVid.classList.remove("sys-init-vid-hidden");
     });
-  }
 
-  const sysClickSuppButton = document.querySelector(".sys-click-supp button");
+    const tlintro = gsap.timeline({
+      scrollTrigger: {
+        trigger: sysContainer,
+        start: "top",
+        end: "300%",
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
 
-  sysClickSuppButton.addEventListener("click", function () {
-    sysClickSupp.classList.add("sys-click-supp-hidden");
-    document.querySelector("body").style.overflow = "";
+    if (sysClickVid.duration) {
+      tlintro
+        .to(sysClickVid, { currentTime: sysClickVid.duration })
+        .call(() => {
+          gsap.to(sysClickVid, {
+            opacity: 0,
+          });
+          sysClickSupp.classList.remove("sys-click-supp-hidden");
+          document.querySelector("body").style.overflow = "hidden";
+        });
+    }
+
+    const sysClickSuppButton = document.querySelector(".sys-click-supp button");
+
+    sysClickSuppButton.addEventListener("click", function () {
+      sysClickSupp.classList.add("sys-click-supp-hidden");
+      document.querySelector("body").style.overflow = "";
+    });
+
+    const tlsupp = gsap.timeline({
+      scrollTrigger: {
+        trigger: sysContainerSupp,
+        start: "top",
+        end: "200%",
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
+
+    tlsupp.to(sysSupVid, { currentTime: sysSupVid.duration }).call(() => {
+      sysErr.classList.remove("sys-supp-err-hidden");
+    });
   });
-
-  const tlsupp = gsap.timeline({
-    scrollTrigger: {
-      trigger: sysContainerSupp,
-      start: "top",
-      end: "300%",
-      scrub: true,
-      pin: true,
-      markers: true,
-    },
-  });
-
-  if (sysClickVid.duration) {
-    console.log(sysSupVid.duration);
-    tlsupp.to(sysSupVid, { currentTime: sysSupVid.duration });
-  }
-
-  // tlsupp.to(sysErr, {
-  //   duration: 5,
-  // });
-
-  // tlsupp.to(sysSupVid, {
-  //   opacity: 0,
-  //   duration: 5,
-  // });
-
-  // gsap.to(sysSupVid, {
-  //   opacity: 0,
-  // });
-  // sysErr.classList.remove("sys-supp-err-hidden");
 });
