b2ada3a009e5a61de5af6921a2cd90ea6e7f23f2
LenouilleLaFripouile <161487400+LenouilleLaFripouile@users.noreply.github.com>
2025-11-20 16:24:05 +0100

    all anim

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,14 @@ window.addEventListener("DOMContentLoaded", function () {
   const sysContainerSupp = document.querySelector(".sys-container-supp");
   const sysSupVid = document.querySelector(".sys-supp-vid");
   const sysErr = document.querySelector(".sys-supp-err");
+  const sysSuppYves = document.querySelector(".sys-supp-yves");
+  const sysSuppDeso = document.querySelector(".sys-supp-deso");
+  const sysSuppSave = document.querySelector(".sys-supp-save");
+  const sysFlash1 = document.querySelector(".sys-flash-1");
+  const sysFlash2 = document.querySelector(".sys-flash-2");
+  const sysFlash3 = document.querySelector(".sys-flash-3");
+  const sysFlash4 = document.querySelector(".sys-flash-4");
+  const sysFlash5 = document.querySelector(".sys-flash-5");
 
   sysSupVid.addEventListener("loadeddata", (e) => {
     sysClickInitButton.addEventListener("click", function () {
@@ -55,15 +63,104 @@ window.addEventListener("DOMContentLoaded", function () {
       scrollTrigger: {
         trigger: sysContainerSupp,
         start: "top",
-        end: "200%",
+        end: "+=500%",
         scrub: true,
         pin: true,
         markers: true,
       },
     });
 
-    tlsupp.to(sysSupVid, { currentTime: sysSupVid.duration }).call(() => {
-      sysErr.classList.remove("sys-supp-err-hidden");
+    tlsupp
+      .to(sysSupVid, {
+        currentTime: sysSupVid.duration,
+        onComplete: () => {
+          sysSupVid.classList.add("sys-supp-vid-hidden");
+        },
+      })
+      .to(sysErr, {
+        opacity: 1,
+      })
+      .to(sysSuppYves, {
+        opacity: 1,
+      })
+      .to(sysErr, {
+        opacity: 0,
+      })
+      .to(
+        sysSuppDeso,
+        {
+          opacity: 1,
+        },
+        "+=0.5"
+      )
+      .to(
+        sysSuppSave,
+        {
+          opacity: 1,
+        },
+        "+=0.5"
+      )
+      .to(sysFlash1, {
+        opacity: 1,
+      })
+      .to(sysFlash2, {
+        opacity: 1,
+      })
+      .to(sysFlash3, {
+        opacity: 1,
+      })
+      .to(sysFlash4, {
+        opacity: 1,
+      })
+      .to(
+        sysFlash5,
+        {
+          opacity: 1,
+        },
+        "+=0.5"
+      );
+
+    const sysUgoContainer = document.querySelector(".sys-ugo-container");
+    const sysUgo = document.querySelector(".sys-ugo");
+    const sysUgoRestore = document.querySelector(".sys-ugo-restore");
+    const sysUgoRestoreClick = document.querySelector(".sys-ugo-restore-click");
+    const sysUgoRestoreClickButton = document.querySelector(
+      ".sys-ugo-restore-click button"
+    );
+    const sysUgoCard = document.querySelector(".sys-ugo-card");
+
+    const tlugo = gsap.timeline({
+      scrollTrigger: {
+        trigger: sysUgoContainer,
+        start: "top",
+        end: "+=500%",
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
+
+    tlugo.to(
+      sysUgo,
+      {
+        opacity: 1,
+      },
+      "+=0.5"
+    );
+    tlugo.to(
+      sysUgoRestore,
+      {
+        opacity: 1,
+      },
+      "+=0.5"
+    );
+    tlugo.to(sysUgoRestoreClick, {
+      opacity: 1,
+    });
+
+    sysUgoRestoreClickButton.addEventListener("click", function () {
+      sysUgoRestoreClick.classList.add("sys-ugo-restore-click-hidden");
+      sysUgoCard.classList.remove("sys-ugo-card-hidden");
     });
   });
 });
